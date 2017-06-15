aab74908c85dc7326913e0efc8d7e2fd290327a6
Alaa Aldeen Subora <ldsubora@outlook.com>
2017-06-15 20:35:54 +0200

    Create run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@ filename <- "getdata_dataset.zip"
 ## Download and unzip the dataset:
 if (!file.exists(filename)){
         fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
-        download.file(fileURL, filename, method="curl")
+        download.file(fileURL, filename)
 }  
 if (!file.exists("UCI HAR Dataset")) { 
         unzip(filename) 
